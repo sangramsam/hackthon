8569c6f3c9ab35e8595cfa1ea83483684f5b5cd1
sangram <singh.sangram56@gmail.com>
2017-06-10 19:32:24 +0530

    auth-token when signup

--- test/www/js/controller/sign-up.js
+++ test/www/js/controller/sign-up.js
@@ -1,7 +1,9 @@
-app.controller('signup', function($scope, $http, $state, signUp) {
+app.controller('signup', function($scope, $http, $state, signUp,auth) {
     $scope.submit = function(userDetails) {
         console.log(userDetails);
         signUp.signUp(userDetails).then(function(response) {
+            var token=response.data.data.user_id;
+            auth.setToken(token);
             $state.go('home');
         })
     }
