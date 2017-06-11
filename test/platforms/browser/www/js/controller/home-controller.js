7d6178cb2fb960fc571525ac4a29f088059b15e7
sangram <singh.sangram56@gmail.com>
2017-06-11 10:45:36 +0530

    auth token validation

--- test/platforms/browser/www/js/controller/home-controller.js
+++ test/platforms/browser/www/js/controller/home-controller.js
@@ -1,3 +1,6 @@
-app.controller('homectrl', function($scope,auth) {
+app.controller('homectrl', function($scope,auth,$state) {
     $scope.token=auth.getToken();
+    if(!auth.getToken()){
+        $state.go('login')
+    }
 });
--- test/www/js/controller/createEvent-controller.js
+++ test/www/js/controller/createEvent-controller.js
@@ -1,4 +1,6 @@
 app.controller('createEvent', function($scope,Login,$state,auth) {
-
+    if(!auth.getToken()){
+        $state.go('login')
+    }
 
 });
