4ecdc5e6c171f25837b1d2a82c0c49e8effbd092
sandeep <sandeeprao69@gmail.com>
2017-06-12 09:10:45 +0530

    changes

--- test/www/js/controller/eventDetails-controller.js
+++ test/www/js/controller/eventDetails-controller.js
@@ -1,21 +1,24 @@
-app.controller('eventDetailsCtrl', function($scope, Login, $state, auth, eventDetails,createEvent) {
+app.controller('eventDetailsCtrl', function($scope, Login, $state, auth, eventDetails, createEvent) {
     eventDetails.getEvent().then(function(res) {
         $scope.event = res.data.data;
-        $scope.eventTime=moment(new Date($scope.event.event_date)).format('D MMM');
+        $scope.eventTime = moment(new Date($scope.event.event_date)).format('D MMM');
     });
-    $scope.comment='';
-$scope.sendComment=function(comment,event_id){
-createEvent.comment(comment,event_id).then(function(response){
-      $scope.comment='';
-     eventDetails.getEvent().then(function(res) {
-        $scope.event = res.data.data;
-    });
-})
-}
-$scope.going=function(id)
-{
-    eventDetails.Going(id).then(function(res){
-        $state.go('category');
-    })
-}
+    $scope.comment = '';
+    $scope.sendComment = function(comment, event_id) {
+        createEvent.comment(comment, event_id).then(function(response) {
+            $scope.comment = '';
+            eventDetails.getEvent().then(function(res) {
+                $scope.event = res.data.data;
+            });
+        })
+    }
+    $scope.going = function(id) {
+        eventDetails.Going(id).then(function(res) {
+            $state.go('category');
+        })
+    }
+    $scope.getdate = function(date) {
+        var date2 = moment(new Date(date)).format('D MMM');
+        return date2;
+    }
 });
